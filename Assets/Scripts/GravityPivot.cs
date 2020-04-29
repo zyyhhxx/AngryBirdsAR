88f75ad14350e3e82c14668eff923f0c4a81f139
Yiyang Zeng <yz3622@nyu.edu>
2020-04-29 16:27:54 -0400

    Adjust gravity based on the image target

--- Assets/Scripts/GravityPivot.cs
+++ Assets/Scripts/GravityPivot.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class GravityPivot : MonoBehaviour
 {
+    public float gravityMagnitude;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gravityMagnitude = Physics.gravity.magnitude;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Physics.gravity);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Physics.gravity = -transform.up * gravityMagnitude;
+        Debug.Log((Physics.gravity, Physics.gravity.magnitude));
     }
 }
